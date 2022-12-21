ec6e2f48c1601ddc0d9053794a027236494f0d77
dziduistkrieg <48360975+dziduistkrieg@users.noreply.github.com>
2022-12-21 18:48:01 +0100

    add solutions to exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let finalString = '';
+    let i = 0;
+    if (number < 0) return "ERROR";
+    else
+    while (i < number) {
+        finalString += string;
+        i++
+    }
+    return finalString;
 };
 
 // Do not edit below this line
