03d2c411d5537fe904b1bfff996e943490614c67
dziduistkrieg <48360975+dziduistkrieg@users.noreply.github.com>
2022-12-27 00:28:47 +0100

    add solution to exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegrees) {
+  return Number(((fahrenheitDegrees - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegrees) {
+  return Number((celsiusDegrees * 9 / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
