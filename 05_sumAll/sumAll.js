440b22887c8015d69eb9ec51b5b1fedc639a3a10
dziduistkrieg <48360975+dziduistkrieg@users.noreply.github.com>
2022-12-26 23:49:23 +0100

    add solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0)
+    return 'ERROR';
+    let sum = 0;
+    if (a > b) {
+        for (let i = b; i <= a; i++){
+            sum += i;
+        }
+    } else {
+        for (let i = a; i <= b; i++){
+            sum += i;
+        }};
+    return sum;
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
