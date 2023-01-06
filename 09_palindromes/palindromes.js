1589a886e1e9d3d729a1f4f0d4a220da1f1adbe9
dziduistkrieg <48360975+dziduistkrieg@users.noreply.github.com>
2023-01-07 00:19:42 +0100

    add solution to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    let onlyLetters = '';
+    for(let i = 0; i < string.length; i++){
+      if(/[a-z]/i.test(string.charAt(i))){
+          onlyLetters += string.charAt(i);
+      }
+    };
+      let reversed = '';
+      for(let i = onlyLetters.length - 1; i >= 0; i--) {
+        reversed += onlyLetters.charAt(i);
+      }
+      return reversed.toLowerCase() === onlyLetters.toLowerCase();
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
