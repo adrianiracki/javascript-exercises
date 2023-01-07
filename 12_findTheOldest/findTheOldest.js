e6b9d43b90dba38f2ca13c3f11fa2a92e17c7ea1
dziduistkrieg <48360975+dziduistkrieg@users.noreply.github.com>
2023-01-07 01:48:37 +0100

    add solutions to exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((person1, person2) => {
+      if(!person1.yearOfDeath) person1.yearOfDeath = (new Date()).getFullYear();
+      if(!person2.yearOfDeath) person2.yearOfDeath = (new Date()).getFullYear();
+      return person1.yearOfDeath - person1.yearOfBirth > person2.yearOfDeath
+      - person2.yearOfBirth ? -1 : 1;
+    })
+    return sortedPeople[0];
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
