4ac444db61b1113969e7fc12abb19167de4ca7b5
dziduistkrieg <48360975+dziduistkrieg@users.noreply.github.com>
2023-01-07 01:10:01 +0100

    add solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    if(n < 0) return "OOPS";
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    let a = 0;
+    let b = 1;
+    let c;
+    for(let i = 2; i <= n; i++){
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return c;
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
